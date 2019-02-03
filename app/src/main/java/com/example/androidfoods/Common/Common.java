4fd2f393ace87d405b442463d25017c4fa9fec94
arab1997 <arab9197#mail.ru>
2019-02-03 16:47:03 +0500

    Foods

---
+++ app/src/main/java/com/example/androidfoods/Common/Common.java
@@ -0,0 +1,11 @@
+package com.example.androidfoods.Common;
+
+import com.example.androidfoods.Model.User;
+
+/**
+ * Created by 123456 on 2017/11/17.
+ */
+
+public class Common {
+    public static User currentUser;
+}
